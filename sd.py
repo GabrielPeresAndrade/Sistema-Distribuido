90d950849ad3814178b42eb6cf20298f098237ac
Gabriel Peres <gabrielperesdeandrade@hotmail.com>
2018-09-27 16:31:30 -0300

    fila bugada nao tira tambem

--- sd.py
+++ sd.py
@@ -5,6 +5,8 @@ import os;
 import socket;
 import threading;
 import struct;
+import asyncio;
+
 def comeco():
     p = Process(target=main, args=());
     p.start();
@@ -21,6 +23,9 @@ def main():
     time.sleep(2);
     #FALAR
     falar()
+        #teste
+    while(fila.empty()!= True):
+        print ('',fila.get_nowait())
     
 
 def falar():
@@ -35,15 +40,24 @@ def falar():
     sf = socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
     ttl = struct.pack('b', 1)
     sf.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL,ttl)
-    #c='{} diz handshack'.format(os.getpid())
+    
+
     c=('clock = %d PID =%d'% (clock,os.getpid()))
+    a =(clock,os.getpid())
     clock = clock +1;
-    sf.sendto(c.encode(),(HOST1,PORT))    
+    sf.sendto(c.encode(),(HOST1,PORT))
+    fila.put_nowait(a)
+
+
     c=('clock = %d PID =%d'% (clock,os.getpid()))
+    a =(clock,os.getpid())
     clock = clock +1;
     sf.sendto(c.encode(),(HOST1,PORT))    
     print('{} # parei de falar\n'.format(os.getpid()));
+    fila.put_nowait(a)
+
     time.sleep(2);
+
 
 def ouvir():
 
@@ -70,10 +84,12 @@ def ouvir():
             print ('Connected with %s PID: %d\n' % (addr,os.getpid())); 
             c='ack'
             sl.sendto(c.encode(),('225.0.0.250',PORT))
-        else:
+        #else:
             #guardar de quem foi o ack para contar
+            #quando chegar a 3 acks do mesmo x , "tirar da fila"
     sl.close()
 
 clock = 0;
+fila = asyncio.PriorityQueue()
 if __name__ == "__main__":
     comeco()
